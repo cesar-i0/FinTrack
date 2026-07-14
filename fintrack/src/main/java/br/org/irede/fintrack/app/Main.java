0fc10ddfaefede8f0a35bd29bc47d9f5f0c98614
cesar.i0 <cesario.0324@hotmail.com>
2026-07-14 13:53:06 -0300

    Versão funcional

--- fintrack/src/main/java/br/org/irede/fintrack/app/Main.java
+++ fintrack/src/main/java/br/org/irede/fintrack/app/Main.java
@@ -5,7 +5,9 @@ import br.org.irede.fintrack.model.TransacaoMensal;
 import br.org.irede.fintrack.utils.TratamentoDeDados;
 import java.time.LocalDate;
 
-
+/**
+ * @author cesar.i0
+ */
 public class Main {
     public static void main(String[] args) {
         FinTrack gerenciador =  new FinTrack();
